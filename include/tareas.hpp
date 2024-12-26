5f38caa0de092b5820f8e68be64f2dc285c7edcb
povnurb <ing.Eduardo.sae.tmx@gmail.com>
2024-12-26 07:03:14 -0600

    operando ok sin detalles

--- include/tareas.hpp
+++ include/tareas.hpp
@@ -167,8 +167,8 @@ void TaskTimeGrafica(void *pvParameters)
     while (1)
     {
         muestra();
-        //vTaskDelay(600000 / portTICK_PERIOD_MS); // para que de 1 minuto portTICK_PERIOD_MS
-        vTaskDelay(60000 / portTICK_PERIOD_MS); // para que de 1 minuto portTICK_PERIOD_MS
+        // vTaskDelay(600000 / portTICK_PERIOD_MS); // para que de 1 minuto portTICK_PERIOD_MS
+        vTaskDelay(120000 / portTICK_PERIOD_MS); // para 2 minuto portTICK_PERIOD_MS
     }
 }
 
